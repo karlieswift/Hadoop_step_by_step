156f607ac8228b15447bf9ae949e757ac87483cf
karlieswift <47564934+karlieswift@users.noreply.github.com>
2020-07-17 15:37:51 +0800

    Update TDao.scala

--- Apache Hadoop/简单实现wordcount框架封装——MVC/MySparkFramwork/src/main/java/com/tay/sparkFramwork/core/TDao.scala
+++ Apache Hadoop/简单实现wordcount框架封装——MVC/MySparkFramwork/src/main/java/com/tay/sparkFramwork/core/TDao.scala
@@ -1,5 +1,8 @@
 package com.tay.sparkFramwork.core
 
+import com.tay.sparkFramwork.util.EnvUtils
+import org.apache.spark.rdd.RDD
+
 /**
  * @author karlieswift 
  *         date: 2020/7/10 17:50 
@@ -8,5 +11,7 @@ package com.tay.sparkFramwork.core
  */
 trait TDao {
 
-  def fromFile(path:String):Any
+  def fromFile(path:String):RDD[String] ={
+      EnvUtils.getEnv().textFile(path)
+  }
 }
